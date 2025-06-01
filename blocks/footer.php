e124f30f78e5ea6e188fbe2efa17b291b6e63e43
Tima056 <2301timofey@gmail.com>
2025-06-01 13:13:08 +0300

    Add files via upload

---
+++ blocks/footer.php
@@ -0,0 +1,20 @@
+<footer>
+        <div class="blocks container">
+            <div>
+                <span class="logo">Design@agency</span>
+            <p>+7 999 999-9999</p>
+            </div>
+        <div>
+            <h4>О нас</h4>
+            <ul>
+                <li>Dprofile</li>
+                <li>Vkontakte</li>
+                <li>Behance</li>
+            </ul>
+        </div>
+            <div>
+            <h4>Расположение</h4>
+            <p>г.Краснодар</p>
+            </div>
+        </div> 
+</footer>
---
+++ blocks/header.php
@@ -0,0 +1,11 @@
+<header class="container">
+    <span class="logo">DesignAgency</span>
+    <nav>
+        <ul>
+            <li><a href="/">Основная страница</a></li>
+            <li><a href="/about.php">О нас</a></li>
+            <li><a href="/auth.php ">Вход</a></li>
+            <li><a href="/reg.php">Регистрация</a></li>
+        </ul>
+    </nav>
+</header>
